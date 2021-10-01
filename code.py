29b0739d2174be8a049bc62fe54b9a0bfa677993
hirunagrad <hirunagrad@gmail.com>
2021-10-01 23:45:27 +0530

    add body of notification

--- code.py
+++ code.py
@@ -15,4 +15,11 @@ if (covidData != None):
         notification.notify(
             #title of the notification,
             title = "COVID19 Stats on {}".format(datetime.date.today()),
+
+#the body of the notification
+    message = "Total cases : {totalcases}\nToday cases : {todaycases}\nToday deaths :{todaydeaths}\nTotal active :{active}".format(
+            totalcases = data['cases'],
+            todaycases = data['todayCases'],
+            todaydeaths = data['todayDeaths'],
+            active = data["active"]),
     
