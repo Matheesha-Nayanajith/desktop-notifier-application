4674b1cdcf8d2a1c8d641fc4905e406c52f540d1
S3cBar0n <59548030+S3cBar0n@users.noreply.github.com>
2021-10-10 11:16:18 -0600

    Cleaned up the notfication a bit, there was no library for the notfication library that was used, so I used Windows 10 Toast.
    Converted the format string to an fstring for better readability.
    
    Wrapped the code within a main function and have it looping for the updates.

--- code.py
+++ code.py
@@ -1,37 +1,42 @@
-#let there is no data initially
-covidData = None
-try:
-    covidData = requests.get("https://corona-rest-api.herokuapp.com/Api/india")
-except:
-    #if the data is not fetched due to lack of internet
-    #reporting data
-    print("Please! Check your internet connection")
+import datetime
+import requests as requests
+from win10toast import ToastNotifier
+import time
 
-if (covidData != None):
-    #converting data into JSON format
-    #converting part
-    data = covidData.json()['Success']
-    
-#repeating the loop for multiple times
-    while(True):
-        notification.notify(
-            #title of the notification,
-            #notification adding
-            title = "COVID19 Stats on {}".format(datetime.date.today()),
 
-#the body of the notification
-    message = "Total cases : {totalcases}\nToday cases : {todaycases}\nToday deaths :{todaydeaths}\nTotal active :{active}".format(
-            totalcases = data['cases'],
-            todaycases = data['todayCases'],
-            todaydeaths = data['todayDeaths'],
-            active = data["active"]),
+def main():
+    toaster = ToastNotifier()
+    covidData = None
+    try:
+        covidData = requests.get("https://corona-rest-api.herokuapp.com/Api/india")
+    except:
+        # if the data is not fetched due to lack of internet
+        # reporting data
+        print("Please! Check your internet connection")
 
-    #creating icon for the notification
-            #we need to download a icon of ico file format
-            app_icon = "Paomedia-Small-N-Flat-Bell.ico",
-            # the notification stays for 50sec
-            timeout  = 50
-)
-        #sleep for 4 hrs => 60*60*4 sec
-        #notification repeats after every 4hrs
-        time.sleep(60*60*4)
+    if (covidData != None):
+        # converting data into JSON format
+        # converting part
+        data = covidData.json()['Success']
+
+        # repeating the loop for multiple times
+        while (True):
+            toaster.show_toast(
+                "COVID19 Stats on {}".format(datetime.date.today()),
+                f"Total cases: {data['cases']}\nToday cases: {data['todayDeaths']}\n"
+                f"Today deaths: {data['todayCases']}\nTotal active: {data['active']}",
+
+                # creating icon for the notification
+                # we need to download a icon of ico file format
+                app_icon="Paomedia-Small-N-Flat-Bell.ico",
+                # the notification stays for 50sec
+                duration=50
+            )
+            # sleep for 4 hrs => 60*60*4 sec
+            # notification repeats after every 4hrs
+
+
+if __name__ == '__main__':
+    while True:
+        main()
+        time.sleep(60 * 60 * 4)
